28d126f00e9f091c5389b47613fc4ec24653408d
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-06 04:35:23 +0530

    Create sizeOfDatatype.java

---
+++ sizeOfDatatype.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(Integer.SIZE/8); 
+        System.out.println(Float.SIZE/8);
+        System.out.println(Short.SIZE/8);
+        System.out.println(Double.SIZE/8);
+        System.out.println(Byte.SIZE/8);
+        System.out.println(Long.SIZE/8);
+      //  System.out.println(Longdouble.SIZE/8);
+        System.out.println(long.SIZE/8);
+    }
+}
