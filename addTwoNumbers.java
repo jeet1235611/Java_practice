2c6d79888be250273611f07e66331144155fe245
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-06 14:58:54 +0530

    Create addTwoNumbers.java

---
+++ addTwoNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+public class inputOutput {
+	public static void main (String args[]) {
+		Scanner s = new Scanner (System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.println(a+b);
+	}
+}
