8bd10b27f12863a3a8bfe2c781c2332603144e9b
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-14 04:55:53 +0530

    Create minMaxValue.java

---
+++ minMaxValue.java
@@ -0,0 +1,9 @@
+public class MyClass {
+     public static void main(String[] args)
+   {
+     System.out.println(Integer.MIN_VALUE);
+     System.out.println(Integer.MAX_VALUE);
+     System.out.println(Integer.MIN_VALUE - 1);
+     System.out.println(Integer.MAX_VALUE + 1);
+   }
+}
