ad2f1a4163f77c7bc27ed0cffba86ba6191192d6
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-23 12:05:49 +0530

    Create string4.java

---
+++ string4.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        char arr[] = { 'c', '0', 'd', 'i', 'n', 'g' };
+        String str = " Jeet ";
+        String str1 = "Coding";
+        String str2 = " is fun";
+        str1 = str1 + str2;
+        System.out.println(str1 + str2);
+        System.out.println(str1.length());
+        System.out.println(str.charAt(2));
+    }
+}
