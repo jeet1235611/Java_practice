aabef030c0666d1e3e0ff7e2e84d3c1933387389
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-28 08:50:39 +0530

    Create ReasonForZeroCodeString.java

---
+++ ReasonForZeroCodeString.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+    
+	Scanner sc = new Scanner(System.in);
+	String str;
+	int i = sc.nextInt();
+	String str2 = sc.nextLine();
+	System.out.println(str2+" "+str2.length());
+    
+    }
+}
