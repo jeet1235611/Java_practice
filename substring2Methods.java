41bce2a461f762027707121e97a6384c2a034ddf
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-28 08:39:54 +0530

    Create substring2Methods.java

---
+++ substring2Methods.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+	char arr[] = {'c', 'o', 'd', 'i', 'n', 'g'};
+	String str1 = "Coding";
+	String substr = str1.substring(1, 4);
+	System.out.println(substr);
+	String substr2 = str1.substring(2);
+	System.out.println(substr2);
+    }
+}
